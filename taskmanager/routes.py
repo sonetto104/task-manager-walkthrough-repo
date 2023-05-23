cc0959796c9b5d5ac7395884fe32be37cc3e3fe9
sonetto104 <peter_regan88@yahoo.com>
2023-05-23 13:48:04 +0000

    Edit out some errors.

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -47,12 +47,13 @@ def add_task():
     categories = list(Category.query.order_by(Category.category_name).all())
     if request.method == "POST":
         task = Task(
-            task_name=form.request.get("task_name"),
-            task_description=form.request.get("task_description"),
+            task_name=request.form.get("task_name"),
+            task_description=request.form.get("task_description"),
             is_urgent=bool(True if request.form.get("is_urgent") else False),
             due_date=request.form.get("due_date"),
-            category_id=request.form.get("category_id"))
+            category_id=request.form.get("category_id")
+        )
         db.session.add(task)
         db.session.commit()
-        return redirect(url_for("categories"))
+        return redirect(url_for("home"))
     return render_template("add_task.html", categories=categories)

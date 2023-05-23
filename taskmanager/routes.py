ee75f08f9f91a266c8edaa997a833ab51fffe6fc
sonetto104 <peter_regan88@yahoo.com>
2023-05-23 07:50:21 +0000

    Trying to use same workspace condiditions as before.

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -2,6 +2,7 @@ from flask import render_template, request, redirect, url_for
 from taskmanager import app, db
 from taskmanager.models import Category, Task
 
+
 @app.route("/")
 def home():
     return render_template("tasks.html")
@@ -13,7 +14,7 @@ def categories():
     return render_template("categories.html", categories=categories)
 
 
-@app.route("/add_category", methods=['GET', 'POST'])
+@app.route("/add_category", methods=["GET", "POST"])
 def add_category():
     if request.method == "POST":
         category = Category(category_name=request.form.get("category_name"))
@@ -23,12 +24,19 @@ def add_category():
     return render_template("add_category.html")
 
 
-@app.route("/edit_category/<int:category_id>", methods=['GET', 'POST'])
+@app.route("/edit_category/<int:category_id>", methods=["GET", "POST"])
 def edit_category(category_id):
     category = Category.query.get_or_404(category_id)
-    if request.method == "POST" :
+    if request.method == "POST":
         category.category_name = request.form.get("category_name")
-        db.session.add(category)
         db.session.commit()
         return redirect(url_for("categories"))
     return render_template("edit_category.html", category=category)
+
+
+@app.route("/delete_category/<int:category_id>")
+def delete_category(category_id):
+    category = Category.query.get_or_404(category_id)
+    db.session.delete(category)
+    db.session.commit()
+    return redirect(url_for("categories"))

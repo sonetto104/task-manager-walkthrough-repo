d2022b0fea186b2184238c18f99e7c60e7c075f5
sonetto104 <peter_regan88@yahoo.com>
2023-05-23 13:42:04 +0000

    Create add tasks functionality.

--- taskmanager/models.py
+++ taskmanager/models.py
@@ -2,7 +2,7 @@ from taskmanager import db
 
 
 class Category(db.Model):
-    #schema for the category model
+    # schema for the category model
     id = db.Column(db.Integer, primary_key=True)
     category_name = db.Column(db.String(25), unique=True, nullable=False)
     tasks = db.relationship("Task", backref="category", cascade="all, delete", lazy=True)
@@ -13,7 +13,7 @@ class Category(db.Model):
 
 
 class Task(db.Model):
-    #schema for the task model
+    # schema for the task model
     id = db.Column(db.Integer, primary_key=True)
     task_name = db.Column(db.String(50), unique=True, nullable=False)
     task_description = db.Column(db.Text, nullable=False)
